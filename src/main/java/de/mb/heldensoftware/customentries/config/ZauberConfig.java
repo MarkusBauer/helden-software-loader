b8485d38113f3c2c65695a76db9e2f70630bab15
Markus Bauer <s9mkbaue@stud.uni-saarland.de>
2025-01-04 14:27:25 +0100

    Json schema generation

--- src/main/java/de/mb/heldensoftware/customentries/config/ZauberConfig.java
+++ src/main/java/de/mb/heldensoftware/customentries/config/ZauberConfig.java
@@ -1,27 +1,54 @@
 package de.mb.heldensoftware.customentries.config;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.imifou.jsonschema.module.addon.annotation.JsonSchema;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+@JsonClassDescription("Eigener, neuer Zauber")
 public class ZauberConfig {
     @JsonProperty(required = true)
+    @JsonPropertyDescription("Zaubername, darf noch nicht existieren. Beispiel: \"Inarcanitas\"")
     String name;
+
     @JsonProperty(required = true)
+    @JsonPropertyDescription("Steigerungs-Kategorie (\"A\"-\"H\")")
+    @JsonSchema(defaultValue="A", pattern = "^[A-H]$")
     String kategorie;
+
     @JsonProperty(required = true)
+    @JsonPropertyDescription("Merkmale, Kurzfassung und Langfassung möglich. Beispiel: [\"Anti\", \"Kraft\", \"Metamagie\"]")
     ArrayList<String> merkmale;
+
     @JsonProperty(required = true)
+    @JsonPropertyDescription("Probe \"XX/YY/ZZ\", auch \"**\" möglich. Beispiel: \"MU/KL/KL\"")
+    @JsonSchema(defaultValue="XX/YY/ZZ", pattern = "^[A-Z*]{2}/[A-Z*]{2}/[A-Z*]{2}$")
     String probe;
+
     @JsonProperty
+    @JsonPropertyDescription("Proben-Modifikator wie \"+MR\", \"+Mod\", ...")
     String mod = "";
+
     @JsonProperty
+    @JsonPropertyDescription("Quellenangabe im Format \"XXX:<seitenzahl>\". Beispiel: \"LCD:123\"")
     String quelle = "";
+
     @JsonProperty
+    @JsonPropertyDescription("Spielwelten, in denen der Zauber zur Verfügung steht. Beispiel: [\"Aventurien\"]")
+    @JsonSchema(defaultValue="[Aventurien]")
     ArrayList<String> settings = new ArrayList<>(); // TODO default
+
     @JsonProperty
+    @JsonPropertyDescription("Verbreitung in Repräsentationen. Beispiele: \n" +
+            "{\"Mag\": 7}\n" +
+            "{\"Hex(Mag)\": 2}")
     HashMap<String, Integer> verbreitung = new HashMap<>();
+
     @JsonProperty
+    @JsonPropertyDescription("Mögliche Spezialisierungen. Beispiel: [\"Reichweite\", \"Zauberdauer\", \"Variante X\"]")
     ArrayList<String> spezialisierungen = new ArrayList<>();
 }
--- src/test/java/de/mb/heldensoftware/customentries/ConfigTest.java
+++ src/test/java/de/mb/heldensoftware/customentries/ConfigTest.java
@@ -1,12 +1,15 @@
 package de.mb.heldensoftware.customentries;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.imifou.jsonschema.module.addon.AddonModule;
+import com.github.victools.jsonschema.generator.*;
+import com.github.victools.jsonschema.module.jackson.JacksonModule;
+import com.github.victools.jsonschema.module.jackson.JacksonOption;
+import de.mb.heldensoftware.customentries.config.Config;
 import de.mb.heldensoftware.customentries.config.Loader;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
+import java.io.*;
 
 public class ConfigTest {
     @Test
@@ -25,6 +28,22 @@ public class ConfigTest {
             Loader.loadFromJson(r);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testSchema() throws FileNotFoundException, UnsupportedEncodingException {
+        SchemaGeneratorConfigBuilder configBuilder =
+                new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_7, OptionPreset.PLAIN_JSON)
+                        .with(new JacksonModule(JacksonOption.RESPECT_JSONPROPERTY_REQUIRED))
+                        .with(new AddonModule());
+        SchemaGeneratorConfig config = configBuilder.build();
+        SchemaGenerator generator = new SchemaGenerator(config);
+        JsonNode jsonSchema = generator.generateSchema(Config.class);
+
+        System.out.println(jsonSchema.toPrettyString());
+        try (PrintWriter writer = new PrintWriter("extensions.schema.json", "UTF-8")) {
+            writer.println(jsonSchema.toPrettyString());
         }
     }
 }
