dba520785ecf75745a390a50adf423970fde43c1
Markus Bauer <markus.bauer@cispa.saarland>
2025-08-21 00:22:28 +0200

    Sideload all kinds of plugins

--- src/main/java/de/mb/heldensoftware/customentries/PluginSideloader.java
+++ src/main/java/de/mb/heldensoftware/customentries/PluginSideloader.java
@@ -1,6 +1,7 @@
 package de.mb.heldensoftware.customentries;
 
 import helden.plugin.HeldenDatenPlugin;
+import helden.plugin.HeldenPlugin;
 import helden.plugin.HeldenPluginFactory;
 
 import java.io.IOException;
@@ -14,10 +15,15 @@ import java.util.*;
  */
 public class PluginSideloader {
 
-	private static Set<Class<? extends HeldenDatenPlugin>> sideloadedPlugins = new HashSet<>();
+	private static Set<Class<? extends HeldenDatenPlugin>> sideloadedDatenPlugins = new HashSet<>();
+	private static Set<Class<? extends HeldenPlugin>> sideloadedRawPlugins = new HashSet<>();
 
 	public static void addPlugin(Class<? extends HeldenDatenPlugin> plugin){
-		sideloadedPlugins.add(plugin);
+		sideloadedDatenPlugins.add(plugin);
+	}
+
+	public static void addRawPlugin(Class<? extends HeldenPlugin> plugin){
+		sideloadedRawPlugins.add(plugin);
 	}
 
 	public static void registerSideloader(){
@@ -32,6 +38,7 @@ public class PluginSideloader {
 	private static void patchHeldenPluginFactory() throws NoSuchMethodException, IOException, InvocationTargetException, IllegalAccessException {
 		InstrumentationEngine inst = new InstrumentationEngine("helden.plugin.HeldenPluginFactory");
 		inst.addMethodInstrumentation("getAlleHeldenDatenPlugins", new InstrumentationEngine.MethodResultAModifier(PluginSideloader.class, "afterGetAlleHeldenDatenPlugins"));
+		inst.addMethodInstrumentation("getAlleHeldenPlugins", new InstrumentationEngine.MethodResultAModifier(PluginSideloader.class, "afterGetAlleHeldenPlugins"));
 		inst.patchClass();
 	}
 
@@ -43,11 +50,32 @@ public class PluginSideloader {
 	 * @return
 	 */
 	public static List<HeldenDatenPlugin> afterGetAlleHeldenDatenPlugins(List<HeldenDatenPlugin> plugins){
-		Set<Class<? extends HeldenDatenPlugin>> remainingPlugins = new HashSet<>(sideloadedPlugins);
+		Set<Class<? extends HeldenDatenPlugin>> remainingPlugins = new HashSet<>(sideloadedDatenPlugins);
 		for (HeldenDatenPlugin p: plugins){
 			remainingPlugins.remove(p.getClass());
 		}
 		for (Class<? extends HeldenDatenPlugin> plugin: remainingPlugins){
+			try {
+				plugins.add(plugin.newInstance());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return plugins;
+	}
+
+    /**
+	 * Patch for {@link HeldenPluginFactory#getAlleHeldenPlugins()}
+	 * Adds sideloaded plugins to list
+	 * @param plugins
+	 * @return
+	 */
+    public static Vector<HeldenPlugin> afterGetAlleHeldenPlugins(Vector<HeldenPlugin> plugins){
+		Set<Class<? extends HeldenPlugin>> remainingPlugins = new HashSet<>(sideloadedRawPlugins);
+		for (HeldenPlugin p: plugins){
+			remainingPlugins.remove(p.getClass());
+		}
+		for (Class<? extends HeldenPlugin> plugin: remainingPlugins){
 			try {
 				plugins.add(plugin.newInstance());
 			} catch (Exception e) {
